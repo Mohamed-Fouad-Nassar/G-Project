488d2632f2a23b740fe84f53fc46b5907df041cf
Mohamed Nassar <93440327+M0-Nassar@users.noreply.github.com>
2022-12-11 15:03:53 +0200

    finish log in page without final colors

--- Front-End/JS Files/test.js
+++ Front-End/JS Files/test.js
@@ -1,9 +1,22 @@
 let ele = document.getElementsByClassName("field");
-
-fieldOne.onfocus = function () {
-  ele[0].style.borderColor = "#aaa";
+let inp = document.getElementsByClassName("input");
+let fIcon = document.getElementsByClassName("f-icon");
+// input 1
+inp[0].onfocus = function () {
+  ele[0].style.cssText = "border-bottom-color: #aaa";
+  fIcon[0].style.cssText = "color: #aaa";
+};
+inp[0].onblur = function () {
+  ele[0].style.cssText = "border-bottom-color: #000";
+  fIcon[0].style.cssText = "color: #000";
 };
 
-fieldOne.onblur = function () {
-  ele[0].style.borderColor = "#000";
+// input 1
+inp[1].onfocus = function () {
+  ele[1].style.cssText = "border-bottom-color: #aaa";
+  fIcon[1].style.cssText = "color: #aaa";
+};
+inp[1].onblur = function () {
+  ele[1].style.cssText = "border-bottom-color: #000";
+  fIcon[1].style.cssText = "color: #000";
 };

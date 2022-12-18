af881a85e28163fb22d2560044232978049b48b2
Mohamed Nassar <93440327+M0-Nassar@users.noreply.github.com>
2022-12-19 00:40:33 +0200

    put the started color test

--- Front-End/JS Files/test.js
+++ Front-End/JS Files/test.js
@@ -3,20 +3,20 @@ let inp = document.getElementsByClassName("input");
 let fIcon = document.getElementsByClassName("f-icon");
 // input 1
 inp[0].onfocus = function () {
-  ele[0].style.cssText = "border-bottom-color: #aaa";
-  fIcon[0].style.cssText = "color: #aaa";
+  ele[0].style.cssText = "border-bottom-color: #ed4342";
+  fIcon[0].style.cssText = "color: #ed4342";
 };
 inp[0].onblur = function () {
-  ele[0].style.cssText = "border-bottom-color: #000";
-  fIcon[0].style.cssText = "color: #000";
+  ele[0].style.cssText = "border-bottom-color: #e67a7a";
+  fIcon[0].style.cssText = "color: #e67a7a";
 };
 
 // input 1
 inp[1].onfocus = function () {
-  ele[1].style.cssText = "border-bottom-color: #aaa";
-  fIcon[1].style.cssText = "color: #aaa";
+  ele[1].style.cssText = "border-bottom-color: #ed4342";
+  fIcon[1].style.cssText = "color: #ed4342";
 };
 inp[1].onblur = function () {
-  ele[1].style.cssText = "border-bottom-color: #000";
-  fIcon[1].style.cssText = "color: #000";
+  ele[1].style.cssText = "border-bottom-color: #e67a7a";
+  fIcon[1].style.cssText = "color: #e67a7a";
 };

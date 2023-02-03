0df00800476379cc78e6c804a53540b5907421b9
Mohamed Nassar <mohammednassar740@gmail.com>
2023-02-04 00:55:15 +0200

    Edit the website all pages

--- Front-End/JS Files/main.js
+++ Front-End/JS Files/main.js
@@ -3,17 +3,18 @@ var mainColor = "#059cb1";
 var whiteColor = "#fff";
 var lightWhiteColor = "#eee";
 var darkTransColor = "#000000d9";
+var lightTransColor = "#ffffffcc";
 
 // fixed nav-bar
 let header = document.getElementById("navbar_top");
 document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", function () {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 150) {
       header.classList.add("fixed-top");
       // add padding top to show content behind navbar
       navbar_height = document.querySelector(".navbar").offsetHeight;
       document.body.style.paddingTop = navbar_height + "px";
-      header.classList.add("navScroll"); //change nav styling
+      // header.classList.add("navScroll"); //change nav styling
     } else {
       document.getElementById("navbar_top").classList.remove("fixed-top");
       // remove padding top from body
@@ -35,36 +36,6 @@ function activeMenu() {
 activeMenu();
 window.addEventListener("scroll", activeMenu);
 
-// nav for small screen
-let navIcon = document.getElementById("navIcon");
-let nav = document.getElementById("nav");
-let ulLinks = document.getElementById("links");
-let logo = document.getElementById("logoText");
-let navIconShape = document.querySelectorAll("#shape");
-navIcon.addEventListener("click", () => {
-  if (navIcon.classList.contains("active")) {
-    nav.style.right = "-100%";
-    nav.style.zIndex = "-1";
-    if (window.pageYOffset === 0)
-      header.style.backgroundColor = lightWhiteColor;
-    else header.style.backgroundColor = darkTransColor;
-    logo.style.color = mainColor;
-    navIcon.classList.remove("active");
-    navIconShape[0].style.backgroundColor = mainColor;
-    navIconShape[1].style.backgroundColor = mainColor;
-    navIconShape[2].style.backgroundColor = mainColor;
-  } else {
-    navIcon.classList.add("active");
-    nav.style.right = "0";
-    nav.style.zIndex = "100";
-    header.style.backgroundColor = mainColor;
-    logo.style.color = whiteColor;
-    navIconShape[0].style.backgroundColor = whiteColor;
-    navIconShape[1].style.backgroundColor = whiteColor;
-    navIconShape[2].style.backgroundColor = whiteColor;
-  }
-});
-
 // back to top button
 let goUp = document.getElementById("up");
 window.onscroll = function () {
@@ -76,26 +47,8 @@ window.onscroll = function () {
     goUp.style.display = "none";
   }
 };
-
 goUp.onclick = function () {
   //go up onclick
   "use strict";
   window.scrollTo(0, 0);
 };
-
-// header in top main color
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset === 0) {
-    if (navIcon.classList.contains("active")) {
-      header.style.backgroundColor = mainColor;
-    } else {
-      header.style.backgroundColor = lightWhiteColor;
-    }
-  } else {
-    if (navIcon.classList.contains("active")) {
-      header.style.backgroundColor = mainColor;
-    } else {
-      header.style.backgroundColor = darkTransColor;
-    }
-  }
-});
--- Front-End/JS Files/test.js
+++ Front-End/JS Files/test.js
@@ -11,12 +11,9 @@ inp[0].onblur = function () {
   fIcon[0].style.cssText = "color: #59c1bd";
 };
 
-// input 2
-inp[1].onfocus = function () {
-  ele[1].style.cssText = "border-bottom-color: #059cb1";
-  fIcon[1].style.cssText = "color: #059cb1";
-};
-inp[1].onblur = function () {
-  ele[1].style.cssText = "border-bottom-color: #59c1bd";
-  fIcon[1].style.cssText = "color: #59c1bd";
-};
+// forget password page cancel button
+let cancelBtn = document.getElementById("cancel");
+cancelBtn.addEventListener("click", function () {
+  console.log("Click");
+  window.history.back();
+});

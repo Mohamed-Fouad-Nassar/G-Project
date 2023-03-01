54cab1b2f3ba93f69f64f599456c105ebebe852a
Mohamed Nassar <mohammednassar740@gmail.com>
2023-03-01 17:50:35 +0200

    remove some directors

--- JS Files/test.js
+++
@@ -1,6 +0,0 @@
-// forget password page cancel button
-let cancelBtn = document.getElementById("cancel");
-cancelBtn.addEventListener("click", function () {
-  console.log("Click");
-  window.history.back();
-});

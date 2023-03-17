f38991cc0581f2bf69051cde17fcae7809bd7881
Mohamed Nassar <mohammednassar740@gmail.com>
2023-03-18 01:26:35 +0200

    upload charity page

---
+++ JS Files/charity.js
@@ -0,0 +1,31 @@
+// events slider
+var swiper = new Swiper(".slider-container", {
+  slidesPerView: 3,
+  spaceBetween: 15,
+  sliderPerGroup: 3,
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
+});
